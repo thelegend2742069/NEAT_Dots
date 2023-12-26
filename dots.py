f2bf9e392e93bc9f3548b31f192dfbe88d1df20d
Pranav Shirole <blab8650@gmail.com>
2023-12-26 22:48:13 +0530

    added goal and stopped updating inactive dots

--- dots.py
+++ dots.py
@@ -11,6 +11,7 @@ WIN_HEIGHT = 600
 # images
 DOT_IMG = pygame.transform.scale(pygame.image.load(os.path.join("assets", "dot.png")), (10, 10))
 BAR_IMG = pygame.image.load(os.path.join("assets", "bar.png"))
+GOAL_IMG = pygame.image.load(os.path.join("assets", "goal.png"))
 BG_IMG = pygame.image.load(os.path.join("assets", "background.png"))
 
 
@@ -22,8 +23,9 @@ class Dots:
 
         self.x = WIN_WIDTH//2 - self.img_width
         self.y = WIN_HEIGHT - self.img_height - 10
-        self.speed = 1
+        self.speed = 2
         self.alive = True
+        self.winner = False
 
     
     
@@ -88,12 +90,47 @@ class Bars:
         return False
 
 
-def draw_window(window: pygame.Surface, dots: list[Dots], bars: list[Bars]) -> None:
+class Goal:
+    def __init__(self) -> None:
+        self.x = WIN_WIDTH//2 - 80
+        self.y = 50
+
+        self.img = GOAL_IMG
+        # self.img_width = self.img.get_width()
+        # self.img_height = self.img.get_height()
+
+    def draw(self, window: pygame.Surface) -> None:
+        window.blit(self.img, (self.x, self.y))
+
+    
+    def get_mask(self) -> pygame.Mask:
+        return pygame.mask.from_surface(self.img)
+
+
+    def collides(self, dot: Dots) -> bool:
+        dot_body = dot.get_mask()
+        goal_body = self.get_mask()
+        offset = (self.x - dot.x, self.y - dot.y)
+
+        if dot_body.overlap(goal_body, offset):
+            print("wins")
+            dot.winner = True
+            return True
+        
+        return False
+
+
+def draw_window(window: pygame.Surface, dots: list[Dots], inactive: list[Dots], bars: list[Bars], goal: Goal) -> None:
     window.blit(BG_IMG, (0, 0))
+    goal.draw(window)
+
     for bar in bars:
         bar.draw(window)
         
     for dot in dots:
+        dot.draw(window)
+
+    for dot in inactive:
         dot.draw(window)
     pygame.display.flip()
 
@@ -106,8 +143,10 @@ set2 = [Bars(200, 150), Bars(-200, 350)]
 
 def main():
     dots = [Dots() for _ in range(1)]
+    inactive = []
     bars = choice([set1, set2])
-    
+    goal = Goal()
+
     window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     clock = pygame.time.Clock()
     game_active = True
@@ -121,6 +160,9 @@ def main():
                 game_active = False
                 pygame.quit()
                 break
+        
+        dead = []
+
         for bar in bars:
             for dot in dots:
                 if dot.alive:
@@ -129,8 +171,17 @@ def main():
                     
                     if bar.collides(dot):
                         dot.kill()
-                
-        draw_window(window, dots, bars)
+                        dead.append(dot)
+
+        for dot in dots:             
+            if goal.collides(dot):
+                dead.append(dot)
+        
+        for dot in dead:
+            dots.remove(dot)
+            inactive.append(dot)
+
+        draw_window(window, dots, inactive, bars, goal)
 
 
 main()
