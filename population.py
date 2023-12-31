c35bd6f67de024c897d81b08c0f159c223807e8e
Pranav Shirole <blab8650@gmail.com>
2023-12-31 10:25:12 +0530

    minor bug fixes

--- population.py
+++ population.py
@@ -4,7 +4,7 @@ class Population:
     def __init__(self, population:int, nodes_per_layer:list) -> None:
         self.population = population
         self.dots = [Dots() for _ in range(self.population)]
-        self.NNs = [NeuralNetwork([nodes_per_layer]) for _ in range(self.population)]
+        self.NNs = [NeuralNetwork(nodes_per_layer) for _ in range(self.population)]
 
 
     def update(self, goal:Goal, bars:list[Bars]):
@@ -15,16 +15,20 @@ class Population:
             inputs = list(map(dot.distance_to, [goal]+bars))
             (up, right, left) = nn.calculate_output(inputs)
             
-            if up > 1: dot.move_up()
-            if right > 1: dot.move_right()
-            if left > 1: dot.move_left()
+            if up > 0.150: dot.move_up()
+            if right > 0.150: dot.move_right()
+            if left > 0.150: dot.move_left()
 
             for bar in bars:
-                if bar.collides(dot): dot.kill()
+                if bar.collides(dot): 
+                    dot.kill()
+                    # print(dot)
+                    # print(nn)
             
             if goal.collides(dot): dot.kill()
     
 
     def draw(self, window):
         for dot in self.dots:
+            # if dot.alive: print("drawing: ", dot, dot.x, dot.y)
             dot.draw(window)
