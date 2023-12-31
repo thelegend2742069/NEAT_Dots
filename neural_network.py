6702699fc0ed929becbb174fe4952069a1d421f0
Pranav Shirole <blab8650@gmail.com>
2023-12-31 10:24:27 +0530

    changed activation function to sigmoid function

--- neural_network.py
+++ neural_network.py
@@ -1,7 +1,10 @@
 import random
+from math import exp
 
 def activation(input):
-    output = max(0, input)
+    # print(input)
+    output = 1/(1+exp(-input)) - 0.5
+    # print(output)
     return output
 
 
@@ -10,6 +13,7 @@ class Layer:
         self.in_nodes = in_nodes
         self.out_nodes = out_nodes
 
+        # print("creating weights")
         if weights == []:
             self.weights = [[random.uniform(-5, 5) for _ in range(out_nodes)] for _ in range(in_nodes)]
         else: self.weights = weights
@@ -18,21 +22,26 @@ class Layer:
             self.biases = [random.uniform(-1, 1) for _ in range(out_nodes)]
         else: self.biases = biases
 
+        print(f'{self.weights=}, {self.biases=}')
+
     
     def calculate_output(self, inputs:list) -> list:
         output = [self.biases[i] for i in range(self.out_nodes)]
 
         for j in range(self.out_nodes):
             for i in range(self.in_nodes):
+                # print(f'{j=}, {inputs[i]=}, {self.weights[i][j]=}')
                 output[j] += inputs[i] * self.weights[i][j]
         
         output = [activation(output[j]) for j in range(self.out_nodes)]
+        print(output)
         return output
 
 
 
 class NeuralNetwork:
     def __init__(self, nodes_per_layer:list) -> None:
+        # print("creating layer")
         self.layers = [Layer(nodes_per_layer[i], nodes_per_layer[i+1]) for i in range(len(nodes_per_layer)-1)]
 
     
